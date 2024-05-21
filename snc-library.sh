66b159584a570ce76c85cdc9c1c8e39833117327
Anjan Nath <kaludios@gmail.com>
2024-05-21 14:03:07 +0530

    update image registry pvc to use topolvm

--- snc-library.sh
+++ snc-library.sh
@@ -140,29 +140,7 @@ function create_json_description {
 }
 
 
-function create_pvs() {
-    local bundle_type=$1
-
-    # Create hostpath-provisioner namespace
-    retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/namespace.yaml
-    # Add external provisioner RBACs
-    retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/external-provisioner-rbac.yaml -n hostpath-provisioner
-    # Create CSIDriver/kubevirt.io.hostpath-provisioner resource
-    retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/csi-driver-hostpath-provisioner.yaml -n hostpath-provisioner
-    # Apply SCC allowin hostpath-provisioner containers to run as root and access host network
-    retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/kubevirt-hostpath-security-constraints-csi.yaml
-
-    # Deploy csi driver components
-    if [[ ${bundle_type} == "snc" ]]; then
-        # in case of OCP we want the images to come from registry.redhat.io
-        # this is done using the kustomize.yaml file
-        retry ${OC} apply -k kubevirt-hostpath-provisioner-csi/csi-driver -n hostpath-provisioner
-    else
-        retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/csi-driver/csi-kubevirt-hostpath-provisioner.yaml -n hostpath-provisioner
-    fi
-
-    # create StorageClass crc-csi-hostpath-provisioner
-    retry ${OC} apply -f kubevirt-hostpath-provisioner-csi/csi-sc.yaml
+function create_registry_pvc() {
 
     # Apply registry pvc with crc-csi-hostpath-provisioner StorageClass
     retry ${OC} apply -f registry_pvc.yaml
